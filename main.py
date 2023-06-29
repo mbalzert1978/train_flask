d9d6835f9056cdb2b821f86df2e5b6cfb9e8143c
mbalzert1978 <teilen-01-koralle@icloud.com>
2023-06-29 09:55:07 +0200

    init

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main() -> None:
-    pass
+from typing import Literal
+
+from flask import Flask
+
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def hello() -> Literal["Hello World!"]:
+    return "Hello World!"
